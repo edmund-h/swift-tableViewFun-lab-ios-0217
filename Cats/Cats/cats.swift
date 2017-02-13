0b8300c69b58a52b4eae8cbd030513ad951faca6
edmund-h <447ght@gmail.com>
2017-02-13 11:16:56 -0500

    learned and implemented alphabetical sections

--- Cats/Cats/cats.swift
+++ Cats/Cats/cats.swift
@@ -9,25 +9,51 @@
 import UIKit
 
 class TableViewController: UITableViewController{
-    let catBreeds = [
-        "Abyssinian","American Bobtail","American Curl","American Shorthair","American Wirehair","Balinese","Bengal","Birman","Bombay","British Shorthair","Burmese","Chartreux","Chausie","Cornish Rex","Devon Rex","Donskoy","Egyptian Mau ","Exotic Shorthair","Havana","Highlander ","Himalayan","Japanese Bobtail","Korat","Kurilian Bobtail","LaPerm","Maine Coon","Manx","Minskin","Munchkin","Nebelung","Norwegian Forest Cat","Ocicat","Ojos Azules","Oriental","Persian","Peterbald","Pixiebob","Ragdoll","Russian Blue","Savannah","Scottish Fold","Selkirk Rex","Serengeti","Siberian","Siamese","Singapura","Snowshoe","Sokoke","Somali","Sphynx","Thai","Tonkinese","Toyger","Turkish Angora","Turkish Van"
-    ]
+    
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+        return alphabet.count
+    } //there should be one section for each letter of the alphabet
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return alphabet
+    } //sections should correspond to letters of the alphabet
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return catBreeds.count
-    }
+        let myKey = alphabet[section]
+        guard let rows = catBreeds[myKey]?.count else { return 0 }
+        return rows
+    } //number of rows in section should be taken from amount of items in array at each letter in dictionary
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "kittyCell", for: indexPath)
-        
-        let myBreed = catBreeds[indexPath.row]
-        
+        let myKey = alphabet[indexPath.section]
+        //⬆️grabs key from alphabet ⬆️ordinally by section
+        let myBreed = catBreeds[myKey]?[indexPath.row]
+        //grabs a cat breed ⬆️ from array corresponding to letter by row
         cell.textLabel?.text = myBreed
-        
+        //sets text of label equal to cat breed
         return cell
-    }
+    }//data in each section row should be taken from the corresponding item in the section letter's array
 }
+
+let catBreeds = [
+    "A": ["Abyssinian","American Bobtail","American Curl","American Shorthair","American Wirehair"],
+    "B": ["Balinese","Bengal","Birman","Bombay","British Shorthair","Burmese"],
+    "C": ["Chartreux","Chausie","Cornish Rex"],
+    "D": ["Devon Rex","Donskoy"],
+    "E": ["Egyptian Mau ","Exotic Shorthair"],
+    "H": ["Havana","Highlander ","Himalayan"],
+    "J": ["Japanese Bobtail"],
+    "K": ["Korat","Kurilian Bobtail"],
+    "L": ["LaPerm"],
+    "M": ["Maine Coon","Manx","Minskin","Munchkin"],
+    "N": ["Nebelung","Norwegian Forest Cat"],
+    "O": ["Ocicat","Ojos Azules","Oriental"],
+    "P": ["Persian","Peterbald","Pixiebob"],
+    "R": ["Ragdoll","Russian Blue"],
+    "S": ["Savannah","Scottish Fold","Selkirk Rex","Serengeti","Siberian","Siamese","Singapura","Snowshoe","Sokoke","Somali","Sphynx"],
+    "T": ["Thai","Tonkinese","Toyger","Turkish Angora","Turkish Van"],
+]
+
+let alphabet = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
